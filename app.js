c483ac3eb7e6bb7d4df2e5238b1ce0cc0826772e
Luke Codewalker <arathorn089@arcor.de>
2018-02-26 22:11:25 +0100

    works but is super messy

--- app.js
+++ app.js
@@ -58,4 +58,65 @@ analyzeBtn.addEventListener('click', () => {
     }
     return indices;
   }, []);
-})
+
+  const selectColumns = (table, indices, header) => {
+    const rows = table.slice(header ? 0 : 2);
+    return rows.map((row, i) => {
+      return row.filter((val, i) => {
+        if (indices.find(index => index === i)) {
+          return val;
+        }
+      })
+    })
+  }
+
+  const reducedTable = selectColumns(data, relevantIndices, false);
+
+  const makeMapMaker = (bgImage, resultContainer) => (row) => {
+    console.log(row);
+    const canvas = document.createElement("canvas");
+    canvas.width = 1200;
+    canvas.height = 900;
+
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(bgImage, 0, 0, 1200, 900);
+
+    let maxX = row[0];
+    let maxY = row[1];
+
+    let scaledX = [];
+    let scaledY = [];
+
+    for (let j = 2; j < row.length; j += 2) {
+      if (!isNaN(row[j])) {
+        scaledX.push(row[j] / maxX);
+        scaledY.push(row[j + 1] / maxY);
+      }
+    }
+
+    for (let k = 0; k < scaledX.length; k++) {
+      ctx.beginPath();
+      let r = k + 1;
+      ctx.ellipse(scaledX[k] * canvas.width, scaledY[k] * canvas.height, 8 / (r / (r + 3)), 8 / (r / (r + 3)), 0, 0, 2 * Math.PI);
+      ctx.fillStyle = "rgba(255,200,0,0.75)";
+      ctx.fill();
+      ctx.strokeStyle = "rgb(255,0,0)";
+      ctx.lineWidth = 3;
+      ctx.stroke();
+
+      ctx.fillStyle = "black";
+      ctx.font = "16px sans-serif bold";
+      ctx.fillText(r, scaledX[k] * canvas.width - 4, scaledY[k] * canvas.height + 8);
+    }
+
+    resultContainer.appendChild(canvas);
+  }
+
+  const makeMap = makeMapMaker(document.querySelector('#source-img'), document.querySelector('#results'));
+
+
+  reducedTable.forEach((row) => {
+    makeMap(row);
+  });
+});
+
